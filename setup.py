768dbcdf43e8c7957af05b8673f78a60e0ed9f05
smallpythoncode <smallpythoncode@gmail.com>
2020-08-27 06:24:50 -0500

    Added: setup.py Deleted: requirments.txt (repo) Updated: .gitignore

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fxdr",
+    version="0.0.0",
+    author="smallpythoncode",
+    author_email="smallpythoncode@gmail.com",
+    description="Does nothing.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/smallpythoncode/fxdr",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "certifi",
+        "chardet",
+        "idna",
+        "oandapyV20",
+        "requests",
+        "six",
+        "ujson",
+        "urllib3",
+        "v20",
+    ],
+
+
+
+)
